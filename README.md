b9af7f512b6eac613d2d09bacca32add90fe1424
bessache <bessache@outlook.com>
2024-02-10 08:46:17 -0300

    Aula 3 Implemantando rotas - Criação de Transações

---
+++ README.md
@@ -0,0 +1,12 @@
+# RF
+[] O usuário deve poder criar una nova transaçao; 
+[] O usuário deve poser obter un resumo da sua conta; 
+[] O usuário deve poder listar todas transaçoes que já ocorreru; 
+[] O usuário deve poder visuaUzar una transação única; 
+
+# RN
+[] A transaçao pode ser do tipo crédito ove sonará ao valor total, o débito subtrairá; 
+[] Deve ser possivel identificarmos o usuário entre as requisições; 
+[] O usuário só pode visualizar transaçóes o qual ere criou;
+
+# RNF
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,12 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
+
 import { env } from './env'
+import { transactionsRoute } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-  return transaction
+app.register(transactionsRoute, {
+  prefix: 'transactions',
 })
 app
   .listen({

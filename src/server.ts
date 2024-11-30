587ba3b74f3865a309ac54f0d3ec2c34a10a3b9f
Renato Bessa <bessache@outlook.com>
2024-11-30 12:58:11 -0300

    commit fim aula tetste automatizados criando uma api rest com nodejs

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
-import fastify from 'fastify'
-
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoute } from './routes/transactions'
-import cookie from '@fastify/cookie'
 
-const app = fastify()
-app.register(cookie)
-
-app.register(transactionsRoute, {
-  prefix: 'transactions',
-})
 app
   .listen({
     port: env.PORT,
